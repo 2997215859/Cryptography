f184ff0969aedfbc3a9ee231485b33088441b10f
DESKTOP-O6VHCR8\ruiy~~ <2997215859@qq.com>
2018-06-02 22:17:26 +0800

    feat: 构建Qt和Cryptograph的开发环境

--- Cryptography/main.cpp
+++ Cryptography/main.cpp
@@ -1,11 +1,35 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <Cryptopp/randpool.h>
+#include <Cryptopp/rsa.h>
+#include <Cryptopp/hex.h>
+#include <Cryptopp/files.h>
+#include <Cryptopp/osrng.h>
+
+#pragma comment(lib, "cryptlib.lib")
+
+using namespace std;
+using namespace CryptoPP;
+
+//------------------------
+// 生成RSA密钥对
+//------------------------
+void GenerateRSAKey(unsigned int keyLength, const char *privFilename, const char *pubFilename, const char *seed)
+{
+    AutoSeededRandomPool rnd;
+    RSA::PrivateKey rsaPrivate;
+    rsaPrivate.GenerateRandomWithKeySize(rnd, 3072);
+    RSA::PublicKey rsaPublic(rsaPrivate);
+}
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+
 
     return a.exec();
 }
