ce86839ee238da5d2bd37cf90e5434adcb70d2d2
DESKTOP-O6VHCR8\ruiy~~ <2997215859@qq.com>
2018-06-02 12:11:23 +0800

    feat: development construct

--- Cryptography/main.cpp
+++ Cryptography/main.cpp
@@ -1,10 +1,11 @@
-#include "cryptography.h"
-#include <QtWidgets/QApplication>
+#include "mainwindow.h"
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	Cryptography w;
-	w.show();
-	return a.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
